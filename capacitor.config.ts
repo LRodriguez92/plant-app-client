aeeb911d822bb0d57f05ee20b681504212a3b881
Leo <leonardo.rodriguez92@gmail.com>
2022-09-01 14:13:44 -0400

    change appId to preview on ios

--- capacitor.config.ts
+++ capacitor.config.ts
@@ -1,10 +1,10 @@
-import { CapacitorConfig } from '@capacitor/cli';
+import { CapacitorConfig } from "@capacitor/cli";
 
 const config: CapacitorConfig = {
-  appId: 'io.ionic.starter',
-  appName: 'plant-app-client',
-  webDir: 'build',
-  bundledWebRuntime: false
+  appId: "com.leo-plant.app",
+  appName: "plant-app-client",
+  webDir: "build",
+  bundledWebRuntime: false,
 };
 
 export default config;
