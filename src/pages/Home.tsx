3acfa64bcd0e20dcef8f7f6b3d14cc06693b68a6
Leo <leonardo.rodriguez92@gmail.com>
2022-09-01 15:39:05 -0400

    make Home scrollable

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -1,12 +1,15 @@
 import Weather from "../components/Weather";
 import Schedule from "../components/Schedule";
+import { IonContent } from "@ionic/react";
 
 const Home: React.FC = () => {
   return (
-    <div className="h-full flex flex-col justify-between mt-10">
-      <Weather />
-      <Schedule />
-    </div>
+    <IonContent style={{ "--background": "transparent" }}>
+      <div className="h-full flex flex-col justify-between mt-10">
+        <Weather />
+        <Schedule />
+      </div>
+    </IonContent>
   );
 };
 
